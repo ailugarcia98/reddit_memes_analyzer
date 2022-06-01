be27536eb2ea45866560b6fa539f10a1b03adfe1
ailugarcia98 <ailu_tunci@hotmail.com>
2022-06-01 19:08:18 -0300

    Añado filter deleted removed

---
+++ workers/filter_deleted_removed/filter_deleted_removed.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+import pika
+import time
+import json
+import logging
+
+class FilterDeletedRemoved:
+    def __init__(self, queue_to_read, queues_to_write):
+        self.queue_to_read = queue_to_read
+        self.queues_to_write = queues_to_write
+
+    def start(self):
+        time.sleep(50)
+
+        connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host='rabbitmq'))
+        channel = connection.channel()
+
+        channel.queue_declare(queue=self.queue_to_read, durable=True)
+        for queue in self.queues_to_write:
+            channel.queue_declare(queue=queue, durable=True)
+
+        channel.basic_qos(prefetch_count=1)
+        channel.basic_consume(queue=self.queue_to_read, on_message_callback=self.callback, auto_ack=True)
+        channel.start_consuming()
+
+    def callback(self, ch, method, properties, body):
+        comments = json.loads(body)
+        if comments == {}:
+            new_body = json.dumps({})
+            for queue in self.queues_to_write:
+                ch.basic_publish(
+                    exchange='',
+                routing_key=queue,
+                body=new_body,
+                properties=pika.BasicProperties(
+                    delivery_mode=2,  # make message persistent
+                ))
+        else:
+            for comment in comments:
+                comments_not_deleted_removed = self.new_body(comment)
+                if comments_not_deleted_removed == "removed/deleted":
+                    continue
+                for queue in self.queues_to_write:
+                    ch.basic_publish(
+                        exchange='',
+                        routing_key=queue,
+                        body=comments_not_deleted_removed,
+                        properties=pika.BasicProperties(
+                            delivery_mode=2,  # make message persistent
+                        ))
+
+    def new_body(self, comment):
+        if comment["body"]=="[removed]" or comment["body"]=="[deleted]":
+            new_comment = "removed/deleted"
+        else:
+            new_comment = comment
+        return json.dumps(new_comment)
+
+
---
+++ workers/filter_deleted_removed/main.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+import logging
+import os
+from filter_deleted_removed import FilterDeletedRemoved
+
+def initialize_config():
+    config_params = {}
+    try:
+        config_params["queue_to_read"] = os.environ["QUEUE_TO_READ"]
+        config_params["queues_to_write"] = [os.environ["QUEUES_TO_WRITE"]] #add split
+    except KeyError as e:
+        raise KeyError("Key was not found. Error: {} .Aborting".format(e))
+    except ValueError as e:
+        raise ValueError("Key could not be parsed. Error: {}. Aborting".format(e))
+
+    return config_params
+
+def main():
+    initialize_log()
+    config_params = initialize_config()
+    filter = FilterDeletedRemoved(config_params["queue_to_read"], config_params["queues_to_write"])
+    filter.start()
+
+def initialize_log():
+    """
+    Python custom logging initialization
+    Current timestamp is added to be able to identify in docker
+    compose logs the date when the log has arrived
+    """
+    logging.basicConfig(
+        format='%(asctime)s %(levelname)-8s %(message)s',
+        level=logging.INFO,
+        datefmt='%Y-%m-%d %H:%M:%S',
+    )
+
+if __name__== "__main__":
+    main()
